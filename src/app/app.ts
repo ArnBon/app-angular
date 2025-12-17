580e75e4be5483a9e6fe55fbb324b23a6299b3ad
Arnaldo Jose Bonillo Berrios <NM41126@banvenez.corp>
2025-12-17 13:38:06 -0400

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('app-angular');
+  protected readonly title = signal('Hola Mundo ! Arnaldo Bonillo');
 }
